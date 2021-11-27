e28ac47c02692c23851b8e20b7f36ddf521ec5f4
Zaid-Elajouri <78742812+Zaid-Elajouri@users.noreply.github.com>
2021-11-27 01:07:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function get_days(){
     var day2 = document.getElementById("d2").value;
     const date_one = new Date(day1);
     const date_two = new Date(day2);
-    const time = Math.abs(date_two - date_one);
+    const time = Math.abs(date_two - date_one);//Math.abs returns the absolute value of the operation,Ex: the absolute value of -8 is the same as the absolute value of 8
     const days = Math.ceil(time/(1000*60*60*24));//converting time calculated from milliseconds to days
                                                  //Math.ceil method rounds a number to the nearest integer value
     document.getElementById("result").innerHTML ="Number of days between the two dates is *"+days+"* day";
